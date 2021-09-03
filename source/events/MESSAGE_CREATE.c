ac1d31a98cef8b70508485133c7c948ac444aa7b
mecura12 <mecura12@gmail.com>
2021-09-04 00:12:04 +0300

    feat: added minimal command handler

--- source/events/MESSAGE_CREATE.c
+++ source/events/MESSAGE_CREATE.c
@@ -1,3 +1,35 @@
+#include <stdbool.h>
+
 #include <jansson.h>
 
-void MESSAGE_CREATE(const json_t *data) {}
+#include "../../configuration.c"
+
+void MESSAGE_CREATE(const json_t *data) {
+  const char *content = json_string_value(json_object_get(data, "content"));
+
+  size_t prefix_length = strlen(config.prefix);
+
+  bool has_prefix = true;
+  short i = 0;
+
+  for (i = 0; i < prefix_length; i++) {
+    if (config.prefix[i] != content[i]) {
+      has_prefix = false;
+      break;
+    }
+  }
+
+  if (has_prefix) {
+    i = 0;
+    size_t content_length = strlen(content);
+    size_t offset_length = content_length;
+    char command[content_length - prefix_length];
+
+    while (offset_length != prefix_length) {
+      command[content_length - offset_length] = content[content_length - offset_length + prefix_length];
+      offset_length -= 1;
+    }
+
+    command[content_length - prefix_length] = '\0';
+  }
+}
